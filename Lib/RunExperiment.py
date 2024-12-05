77113a4ccc79d62a369ba75aacc7e72c2de9df9a
Danial Noori Zadeh <noorizad@mcmaster.ca>
2024-12-05 18:25:33 -0500

    Created generic transfer functions for Sara

--- Lib/RunExperiment.py
+++ Lib/RunExperiment.py
@@ -4,45 +4,44 @@ from Experiment import SymbolixExperimentCG
 
 _output = [Vop, Von]
 _input  = [Iip, Iin]
-T_type  = "simple"
-experimentName = "TIA"
-experimentName += "_" + T_type
+T_types  = ["simple", "some_parasitic", "full_parasitic"]
 
-baseTF = BaseTransferFunction(_output, _input,
-                              transmissionMatrixType=T_type)
-baseTF.solve()
+for T_type in T_types:
+    experimentName = "TIA"
+    experimentName += "_" + T_type
 
-impedanceKeys = [
-            "Z1_ZL",
-            "Z2_ZL",
-            "Z3_ZL",
-            "Z4_ZL",
-            "Z5_ZL",
-            "Z1_Z2_ZL",
-            "Z1_Z3_ZL",
-            "Z1_Z4_ZL",
-            "Z1_Z5_ZL",
-            "Z2_Z3_ZL",
-            "Z2_Z4_ZL",
-            "Z2_Z5_ZL",
-            "Z3_Z4_ZL",
-            "Z3_Z5_ZL",
-            "Z4_Z5_ZL"
-            ]
+    baseTF = BaseTransferFunction(_output, _input,
+                                transmissionMatrixType=T_type)
+    baseTF.solve()
 
-# Z_arr_list = Z_arr_list[0:2]
-# impedanceKeys = ["Z5_ZL"]
+    impedanceKeys = [
+                "Z1_ZL",
+                "Z2_ZL",
+                "Z3_ZL",
+                "Z4_ZL",
+                "Z5_ZL",
+                "Z1_Z2_ZL",
+                "Z1_Z3_ZL",
+                "Z1_Z4_ZL",
+                "Z1_Z5_ZL",
+                "Z2_Z3_ZL",
+                "Z2_Z4_ZL",
+                "Z2_Z5_ZL",
+                "Z3_Z4_ZL",
+                "Z3_Z5_ZL",
+                "Z4_Z5_ZL"
+                ]
 
-for i, key in enumerate(impedanceKeys, 1):
-    print(f"==> Running the {experimentName} Experiment for {key} ({i}/{len(impedanceKeys)})\n")
-    experiment = SymbolixExperimentCG(experimentName, baseTF)
-    experiment.computeTFs(comboKey=key)
-    #
-    experiment.classifier.classifyBiQuadFilters()
-    experiment.classifier.summarizeFilterType()
+    # Z_arr_list = Z_arr_list[0:2]
+    # impedanceKeys = ["Z5_ZL"]
 
-    experiment.reportSummary(experimentName, key)
-    experiment.compilePDF()
+    for i, key in enumerate(impedanceKeys, 1):
+        print(f"==> Running the {experimentName} Experiment for {key} ({i}/{len(impedanceKeys)})\n")
+        experiment = SymbolixExperimentCG(experimentName, baseTF)
+        experiment.computeTFs(comboKey=key)
+        #
+        experiment.classifier.classifyBiQuadFilters()
+        experiment.classifier.summarizeFilterType()
 
-print(impedanceKeys)
-experiment.classifier.impedanceList
+        experiment.reportSummary(experimentName, key)
+        experiment.compilePDF()
