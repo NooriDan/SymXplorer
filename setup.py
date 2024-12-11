df28b9926b48157fccf229d5d7ff97a96449bd02
Danial Noori Zadeh <noorizad@mcmaster.ca>
2024-12-11 12:37:23 -0500

    created a setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "MacAnalog-Symbolix",
+    description = "A python app to perform symbolic analysis on analog circuits", 
+    version = "0.1",
+    packages = find_packages(where="MacAnalog-Symbolix")
+)
