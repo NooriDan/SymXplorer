25b29c3b6f6e7b55002740476f5efe0a22c2033a
Danial Noori Zadeh <noorizad@mcmaster.ca>
2025-09-14 07:12:27 +0200

    passed sanity on the IHP design

--- examples/tunable-tia/scripts/optimizer_application.py
+++ examples/tunable-tia/scripts/optimizer_application.py
@@ -7,5 +7,6 @@ print ("Spicelib_Wrapper imported successfully")
 
 
 if __name__ == "__main__":
-    wrapper = Spicelib_Wrapper(Path("/foss/designs/eda/SymXplorer/examples/tunable-tia/tia-bpf-1/netlist/tb_ac.spice"))
+    netlist_path = "/foss/designs/eda/SymXplorer/examples/tunable-tia/ihp-sg13g2/spice/tb_ac.spice"
+    wrapper = Spicelib_Wrapper(Path(netlist_path))
     wrapper.run_sanity_check(use_editor=True)
